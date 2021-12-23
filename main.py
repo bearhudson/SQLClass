7608d016fc0294119b32a198b6157c2bcaf59278
Brian Hudson <reallybrianhudson@gmail.com>
2021-12-22 20:43:36 -0500

    Better

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ class SQLClass:
     def return_test(self):
         return self.mysql_db, self.mysql_username, self.mysql_host
 
+    def __str__(self):
+        return_string = f"{mysql_host}, {mysql_db}"
+        return return_string
+
 
 sql = SQLClass(mysql_db=mysql_db, mysql_host=mysql_host, mysql_username=mysql_username, mysql_pass=mysql_pass)
 print(sql.return_test())
+print(sql)
