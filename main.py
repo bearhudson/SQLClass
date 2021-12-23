cdef493d355257d5646ccbd974db1a13d001e00e
Brian Hudson <reallybrianhudson@gmail.com>
2021-12-22 20:30:47 -0500

    Second Commit

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-import dataclasses
+from dataclasses import dataclass
+from src.environs import *
 
+
+@dataclass
+class SQLClass:
+    mysql_db: str
+    mysql_host: str
+    mysql_username: str
+    mysql_pass: str
+
+    def return_test(self):
+        return self.mysql_db, self.mysql_username, self.mysql_host
+
+
+sql = SQLClass(mysql_db=mysql_db, mysql_host=mysql_host, mysql_username=mysql_username, mysql_pass=mysql_pass)
+print(sql.return_test())
